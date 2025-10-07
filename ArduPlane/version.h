e437af598dcd75b0d2447cedd4155fa72bca291b
Taras Lishchenko <taras.lishchenko@gmail.com>
2025-10-07 07:45:14 +0000

    added non-gps takeoff and version

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.6.2"
+#define THISFIRMWARE "ArduPlane TA V4.6.2"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,6,2,FIRMWARE_VERSION_TYPE_OFFICIAL
