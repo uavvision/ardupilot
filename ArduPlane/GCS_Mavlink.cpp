df8c62553fe5711e136335be0971c7450fbcd1a4
Taras Lishchenko <taras.lishchenko@gmail.com>
2025-12-22 16:37:21 +0000

    Revert "added septentrio anti-spoofing support"
    This reverts commit d504916f871b57f5edba57bebdef77d7258f684c.

--- ArduPlane/GCS_Mavlink.cpp
+++ ArduPlane/GCS_Mavlink.cpp
@@ -698,9 +698,6 @@ static const ap_message STREAM_EXTRA3_msgs[] = {
 #endif
     MSG_EKF_STATUS_REPORT,
     MSG_VIBRATION,
-#if AP_MAVLINK_MSG_GNSS_INTEGRITY_ENABLED
-    MSG_GNSS_INTEGRITY,
-#endif  // AP_MAVLINK_MSG_GNSS_INTEGRITY_ENABLED
 };
 static const ap_message STREAM_PARAMS_msgs[] = {
     MSG_NEXT_PARAM
