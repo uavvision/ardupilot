878c2240b2249e0d01bcd83e3c9d5a5277c6f398
Taras Lishchenko <taras.lishchenko@gmail.com>
2026-01-08 15:49:28 +0000

    fixed version

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane TA[B|T|2G] V4.5.4 Rev. 08-12-26"
+#define THISFIRMWARE "ArduPlane TA[B|T|2G] V4.5.4 Rev. 08-01-26"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,5,4,FIRMWARE_VERSION_TYPE_OFFICIAL
